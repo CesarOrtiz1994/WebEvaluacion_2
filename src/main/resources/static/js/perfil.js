31fb46a1b0c24e3cdf7daf836d0b904d1bf328d0
CesarOrtiz1994 <cesarortize94@gmail.com>
2023-08-16 08:49:10 -0600

    cabios explicacion

--- src/main/resources/static/js/perfil.js
+++ src/main/resources/static/js/perfil.js
@@ -8,9 +8,6 @@ window.addEventListener('load', function() {
 
 async function verPerfil(correo) {
     let api_url = "http://localhost:8080";
-    var script = document.querySelector('script[src^="/js/perfil.js"]'); // obtiene el elemento script que tiene el atributo src que empieza con "/js/perfil.js"
-    var url = script.src; // obtiene el valor del atributo src
-    var userName = url.split('?')[1]; // obtiene la parte después del signo de interrogación
 
     let footerModal = document.getElementById("footerModal");
 
@@ -21,8 +18,8 @@ async function verPerfil(correo) {
                 imagen.src = '/img/' + perfil.foto;
                 imagen.alt = '/img/' + perfil.foto;
                 document.getElementById('nombres').value = perfil.nombres;
-                document.getElementById('perf-apellidos').value = perfil.apellidos;
-                document.getElementById('perf-correo').innerHTML = perfil.correo;
+                document.getElementById('apellidos').value = perfil.apellidos;
+                document.getElementById('perf-correo').value = perfil.correo;
                 document.getElementById('perf-fechaNac').innerHTML = perfil.fechaNac;
                 document.getElementById('perf-carrera').innerHTML = perfil.carrera;
                 document.getElementById('perf-prepa').innerHTML = perfil.prepa;
@@ -37,7 +34,7 @@ async function verPerfil(correo) {
                 document.getElementById('perf-sitSentimental').innerHTML = perfil.sitSentimental;
                 document.getElementById('perf-gustos').innerHTML = perfil.gustos;
 
-                if (perfil.correo == userName) {
+                if (perfil.correo == correo) {
                     footerModal.innerHTML = `<a class="btn btn-outline-primary" href="/perfil/editar">Editar</a>
                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>`;
                 } else {
